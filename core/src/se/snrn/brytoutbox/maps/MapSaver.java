73832746faaba53b871db90f589e993f80424fc2
sistergodiva <fredriksson.pontus@gmail.com>
2017-04-26 08:28:40 +0200

    added http get/post for highscores

--- core/src/se/snrn/brytoutbox/maps/MapSaver.java
+++ core/src/se/snrn/brytoutbox/maps/MapSaver.java
@@ -1,26 +1,26 @@
-package se.snrn.brytoutbox.maps;
-
-
-import com.badlogic.gdx.utils.Json;
-import com.badlogic.gdx.utils.JsonWriter;
-
-import java.io.FileWriter;
-import java.io.IOException;
-
-public class MapSaver {
-
-    public static void saveMap(int[][] mapArray, String name){
-        try {
-            JsonWriter jsonWriter = new JsonWriter(new FileWriter("maps/"+name+".json"));
-            Json json = new Json();
-            json.setWriter(jsonWriter);
-            json.writeValue(MapLoader.getRandomGrid());
-            jsonWriter.flush();
-            jsonWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-}
+//package se.snrn.brytoutbox.maps;
+//
+//
+//import com.badlogic.gdx.utils.Json;
+//import com.badlogic.gdx.utils.JsonWriter;
+//
+//import java.io.FileWriter;
+//import java.io.IOException;
+//
+//public class MapSaver {
+//
+//    public static void saveMap(int[][] mapArray, String name){
+//        try {
+//            JsonWriter jsonWriter = new JsonWriter(new FileWriter("maps/"+name+".json"));
+//            Json json = new Json();
+//            json.setWriter(jsonWriter);
+//            json.writeValue(MapLoader.getRandomGrid());
+//            jsonWriter.flush();
+//            jsonWriter.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//
+//}
